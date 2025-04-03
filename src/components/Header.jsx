c7fb3e6393732e9872c248e46f92ba845a876b7e
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-03 16:02:22 +0200

    feat: creati e importati file componenti

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return<header>
+        <h1>dc comics</h1>
+    </header>
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+export default function Main(){
+    return <main>
+        <h2>sono il main</h2>
+    </main>
+}
