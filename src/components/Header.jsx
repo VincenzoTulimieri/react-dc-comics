6635f00142aec4dc2b8c26d2615f19a5ae3db3b7
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-03 16:57:39 +0200

    feat: creati file logo e menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,10 @@
+// importazione file
+import HeaderMenu from "./HeaderMenu";
+import Logo from "./logo";
 
 export default function Header() {
     return <header id="intestazione">
-        <div className="image">
-            <img src="../public/img/dc-logo.png" alt="logo" />
-        </div>
-        <ul className="header-menu">
-            <li><a href="#"><strong>characters</strong></a></li>
-            <li><a href="#"><strong>comics</strong></a></li>
-            <li><a href="#"><strong>movies</strong></a></li>
-            <li><a href="#"><strong>tv</strong></a></li>
-            <li><a href="#"><strong>games</strong></a></li>
-            <li><a href="#"><strong>collectibles</strong></a></li>
-            <li><a href="#"><strong>videos</strong></a></li>
-            <li><a href="#"><strong>fans</strong></a></li>
-            <li><a href="#"><strong>news</strong></a></li>
-            <li><a href="#"><strong>shop</strong></a></li>
-        </ul>
+        <Logo />
+        <HeaderMenu />
     </header>
 }
---
+++ src/components/logo.jsx
@@ -0,0 +1,5 @@
+export default function Logo() {
+    return <div className="image">
+        <img src="../public/img/dc-logo.png" alt="logo" />
+    </div>
+}
