d0e220d955915c93a65d223824f004ac84d467cc
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-03 18:55:40 +0200

    feat: aggiunto logo background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
-export default function Footer(){
+export default function Footer() {
     return <footer>
-        
+        <div className="container">
+            <div className="container-img">
+                <img src="../public/img/dc-logo-bg.png" alt="" />
+            </div>
+        </div>
     </footer>
 }
