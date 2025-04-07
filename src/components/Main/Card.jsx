fe872180cd6b9037c1fa95ba8670b228e2a0274f
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-07 18:11:10 +0200

    feat: fatto elemento card

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -7,9 +7,9 @@ export default function Card(props) {
             <div className="col">
                 <article>
                     <div className="card-img">
-                        <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" alt="" />
+                        <a href="#"><img src={comic.thumb} alt={comic.title} /></a>
                     </div>
-                    <p className="card-text">action comics</p>
+                    <a href="#"><p className="card-text">{comic.series}</p></a>
                 </article>
             </div>
                 
